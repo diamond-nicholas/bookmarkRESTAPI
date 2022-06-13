a35bfda925bc8995501058630851193e32877d8f
diamond-nicholas <diamondnicholas154@gmail.com>
2022-06-13 11:32:21 +0100

    add bookmark model, create one-one relationship, create shorten_url logic

--- src/database.py
+++ src/database.py
@@ -1,5 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from sqlalchemy.orm import backref
+from enum import unique
+import string
+import random
 
 db = SQLAlchemy()
 
@@ -10,7 +14,37 @@ class User(db.Model):
     password = db.Column(db.Text(), unique=True, nullable=False)
     created_at= db.Column(db.DateTime,default=datetime.now())
     updated_at = db.Column(db.DateTime, onupdate=datetime.now())
+    bookmarks = db.relationship('Bookmark', backref='user')
 
 
     def __ref__(self) -> str:
         return 'User>>>> {self.username}'
+
+class Bookmark(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text, nullable=True)
+    url = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    short_url = db.Column(db.String(3), nullable=True)
+    visit= db.Column(db.Integer, default=0)
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, onupdate=datetime.now())
+
+    def generate_shortener(self):
+        characters = string.digits+string.ascii_letters
+        picked_char = ''.join(random.choice(characters, k=3))
+        link = self.query.filter_by(short_url=picked_char).first()
+
+        if link:
+            self.generate_shortener()
+        else:
+            return picked_char
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.short_url= self.generate_shortener()
+
+
+
+
