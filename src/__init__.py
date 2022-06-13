157800e3984aa7fa1a444474ba6f832d043a4dc9
diamond-nicholas <diamondnicholas154@gmail.com>
2022-06-13 18:54:54 +0100

    create user login with validations

--- src/__init__.py
+++ src/__init__.py
@@ -1,19 +1,29 @@
 from flask import Flask, jsonify
+# from dotenv import load_dotenv
 import os
 from src.bookmarks import bookmarks
 from src.auth import auth
+from src.database import db
+
+
+# load_dotenv()  # take environment variables from .env.
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bookmarks.db'
+
 
     if test_config is None:
 
         app.config.from_mapping(
-            SECRET_KEY=os.environ.get('SECRET_KEY'),
+            SECRET_KEY=os.environ.get('SECRET_KEY')
+            # SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URIQLALCHEMY_DB_URI'), SQLALCHEMY_TRACK_MODIFICATIONS=False
         )
     else:
         app.config.from_mapping(test_config)
 
+    db.app=app
+    db.init_app(app)
     app.register_blueprint(auth)
     app.register_blueprint(bookmarks)
     return app
--- src/auth.py
+++ src/auth.py
@@ -1,12 +1,47 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from werkzeug.security import check_password_hash, generate_password_hash, gen_salt
+from src.constants.http_status_codes import HTTP_400_BAD_REQUEST, HTTP_409_CONFLICT, HTTP_201_CREATED
+import validators
+from src.database import User, db
+
+
 
 auth = Blueprint("auth", __name__, url_prefix='/api/v1/auth')
 
 @auth.post('/register')
 def register():
-    return 'user created'
+    email = request.json['email']
+    username = request.json['username']
+    password = request.json['password']
 
+    if len(password)< 6:
+        return jsonify({"error": "Password length must be greater than 6 characters"}), HTTP_400_BAD_REQUEST
 
+    if len(username)< 3:
+        return jsonify({"error": "Username length must be greater than 3 characters"}), HTTP_400_BAD_REQUEST
+
+    if not username.isalnum() or " " in username:
+        return jsonify({"error": "User should not have spaces and be alphanumeric"}), HTTP_400_BAD_REQUEST
+
+    if not validators.email(email):
+        return jsonify({"error": "Please enter a valid email account"}), HTTP_400_BAD_REQUEST
+
+    if User.query.filter_by(email=email).first() is not None:
+        return jsonify({"error": "Email is taken"}), HTTP_409_CONFLICT
+
+    if User.query.filter_by(username=username).first() is not None:
+        return jsonify({"error": "Username is taken"}), HTTP_409_CONFLICT
+    pwd_hash=generate_password_hash(password)
+
+    user = User(email=email, username=username, password=pwd_hash)
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({
+        "message": 'User has been successfully created',
+        "username": username,
+        "email": email
+    }), HTTP_201_CREATED
 @auth.get('/me')
 def me():
     return {"user": "me"}
